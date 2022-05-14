24a8e54aea679390f65f31884afe406cd41013e3
SaraAzmin <azmin.sara17@gmail.com>
2022-05-14 23:42:55 +0200

    data loading added

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d2zcy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+
+        await client.connect();
+        const productCollection = client.db('dbecommerce').collection('product');
+
+        app.get('/product', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('server connected');
+});
+
+app.listen(port, () => {
+    console.log('port connected ', port);
+})
